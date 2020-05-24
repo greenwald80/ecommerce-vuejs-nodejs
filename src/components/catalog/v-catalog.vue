6c7b6c75807c6a8c5328e09b0f77454728fae353
Alexander <greenwald80@gmail.com>
2020-05-24 18:19:38 +0300

    Added select

--- src/components/catalog/v-catalog.vue
+++ src/components/catalog/v-catalog.vue
@@ -4,9 +4,11 @@
       <div class="v-catalog__link_to_cart">Cart: {{CART.length}}</div>
     </router-link>
     <h1>Catalog</h1>
+    <v-select v-bind:selected="selected" v-bind:options="categories" @select="sortByCategories" />
+    <hr />
     <div class="v-catalog__list">
       <v-catalog-item
-        v-for="product in PRODUCTS"
+        v-for="product in filteredProducts"
         :key="product.article"
         v-bind:product_data="product"
         @addToCart="addToCart"
@@ -18,23 +20,51 @@
 <script>
 import vCatalogItem from "./v-catalog-item";
 import { mapActions, mapGetters } from "vuex";
+import vSelect from "../v-select";
 
 export default {
   name: "v-catalog",
   props: {},
   components: {
-    vCatalogItem
+    vCatalogItem,
+    vSelect
   },
   data() {
-    return {};
+    return {
+      categories: [
+        { name: "All", value: "all" },
+        { name: "Male", value: "m" },
+        { name: "Female", value: "f" }
+      ],
+      selected: "All",
+      sortedProducts: []
+    };
   },
   computed: {
-    ...mapGetters(["PRODUCTS", "CART"])
+    ...mapGetters(["PRODUCTS", "CART"]),
+    filteredProducts() {
+      if (this.sortedProducts.length) {
+        return this.sortedProducts;
+      } else {
+        return this.PRODUCTS;
+      }
+    }
   },
   methods: {
     ...mapActions(["GET_PRODUCTS_FROM_API", "ADD_TO_CART"]),
     addToCart(data) {
       this.ADD_TO_CART(data);
+    },
+    sortByCategories(category) {
+      this.sortedProducts = []; //чистит изначально отсортированный список
+      let vm = this;
+      this.PRODUCTS.map(function(item) {
+        if (item.category === category.name) {
+          //сравнивает категорию продуктов из бд с выбранной категорией в селекте
+          vm.sortedProducts.push(item);
+        }
+      });
+      this.selected = category.name;//в селекте остается выбранная категория
     }
   },
   mounted() {
